701f19db0e0d8639b2be2d33d10810a64458496f
Pawel Goliszewski <pawlik9.hamster@gmail.com>
2019-05-30 12:49:47 +0200

    During creating rating system

--- static/js/przepis.js
+++ static/js/przepis.js
@@ -2,15 +2,19 @@
     let defaultAmount = [];
     readDefaultAmount();
     let changeAmountBtn = document.querySelector("#changeAmount");
-    changeAmountBtn.addEventListener('click', ()=>{
+    changeAmountBtn.addEventListener('click', () => {
         let scale = document.querySelector("#numberOfPeople");
-        if(scale.value < 1) scale.value = 1;
+        if (scale.value < 1) scale.value = 1;
         changeAmount(scale.value);
     });
-    function changeAmount(scale){
-        document.querySelectorAll('amount').forEach((e, i) => { e.innerHTML = parseInt(defaultAmount[i]) * scale});
+
+    function changeAmount(scale) {
+        document.querySelectorAll('amount').forEach((e, i) => {
+            e.innerHTML = parseInt(defaultAmount[i]) * scale
+        });
     }
-    function readDefaultAmount(){
+
+    function readDefaultAmount() {
         document.querySelectorAll("amount").forEach((e, i) => {
             defaultAmount[i] = e.innerHTML;
         });
